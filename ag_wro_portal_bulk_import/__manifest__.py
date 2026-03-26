179491476b2d595fc55f7e6f9464823809b2f0ea
naman8171 <naman@inomerp.in>
2026-03-26 16:40:14 +0530

    Move WRO portal bulk import into separate addon module

---
+++ ag_wro_portal_bulk_import/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'The Hub WRO Portal Bulk Import',
+    'version': '15.0.1.0.0',
+    'category': 'Customization',
+    'summary': 'Portal bulk import for Warehouse Receiving Orders',
+    'depends': ['ag_the_hub'],
+    'data': [
+        'views/wro_portal_bulk_import_templates.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
