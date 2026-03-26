02b459f7678983518d1da57618b6fd0458fc97c4
naman8171 <naman@inomerp.in>
2026-03-26 17:02:27 +0530

    Avoid XML load error by creating portal inherit view in post-init hook

---
+++ ag_wro_portal_bulk_import/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'The Hub WRO Portal Bulk Import',
+    'version': '15.0.1.0.0',
+    'category': 'Customization',
+    'summary': 'Portal bulk import for Warehouse Receiving Orders',
+    'depends': ['ag_the_hub'],
+    'data': [],
+    'post_init_hook': 'post_init_hook',
+    'uninstall_hook': 'uninstall_hook',
+    'installable': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
